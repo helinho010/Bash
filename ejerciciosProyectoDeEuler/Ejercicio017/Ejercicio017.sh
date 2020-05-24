f40b547beca6bc452fd49efa23b5bfe836c2fca6
Helio Mejia <mm.helio009@gmail.com>
2020-05-23 20:30:43 -0400

    Actualizacion de codigo

--- ejerciciosProyectoDeEuler/Ejercicio017/Ejercicio017.sh
+++ ejerciciosProyectoDeEuler/Ejercicio017/Ejercicio017.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+timeInicio=`date +"%H:%M:%S"`
 suma=0
-for ((i=340; i<=342; i++))  
+for ((i=1; i<=5; i++))  
 do
 curl "https://www.lemoda.net/tools/numbers/index.es.cgi?number=$i" -o numeroIngles.txt 2>Salida.txt
 literal=`cat numeroIngles.txt | head -55 | tail -1 | tr -d " " | tr -d "-"`
 
 cantidadCaracteres=${#literal}
-((suma=suma+cantidadCaracteres))
-echo "El numero $i en literal ingles es: $literal y la cantidad de caracteres es ${#literal}" 
-echo -e "\n \t $suma"
+((suma=suma+$cantidadCaracteres))
 done
+timeFinal=`date +"%H:%M:%S"`
+
+echo -e "tiempo inicio: $timeInicio \n tiempo final: $timeFinal \n"
+echo "La suma total es :::$suma:::"
